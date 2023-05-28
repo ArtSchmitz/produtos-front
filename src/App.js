7b2b246d5c410aa40f0aacb76274d4fd2b29623d
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-05-28 13:43:02 -0300

    conectando back

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const getProdutos = async () => {
+    fetch('http://localhost/produtos-back/index.php')
+    .then((response) => response.json())
+    .then((responseJson) => (
+      console.log(responseJson)
+    ));
+  }
+
+  useEffect(() => {
+    getProdutos();
+  }, [])
+
   return (
-   <h1>Hello World</h1>
+   <h1>Listar</h1>
   );
 }
 
