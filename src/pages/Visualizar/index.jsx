239ce7c6dd8f9475d8fa9d319c03cb78b2a7d72e
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-06-11 21:50:46 -0300

    recebendo api

--- src/pages/Visualizar/index.jsx
+++ src/pages/Visualizar/index.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { useEffect } from "react";
+import React, {useEffect, useState } from "react";
 
-export const Visualizar = () => {
+export const Visualizar = (props) => {
+
+  const [id] = useState(props.match.params.id)
+
   useEffect(() => {
     const getProduto = async () => {
-      await fetch("http://localhost/produtos-back/visualizar.php?")
+      await fetch("http://localhost/produtos-back/visualizar.php?id=" + id)
       .then((response) => response.json())
       .then((responseJson) => {
         console.log(responseJson)
       });
     };
     getProduto();
-  }, []);
+  }, [id]);
   return <h1>Visualizar</h1>;
 };
