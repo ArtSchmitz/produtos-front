de96ec48dd88e1102489c365402b641d6f8fb283
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-06-18 18:55:30 -0300

    retornando produtos

--- src/pages/Visualizar/index.jsx
+++ src/pages/Visualizar/index.jsx
@@ -1,18 +1,22 @@
 import React, {useEffect, useState } from "react";
 
 export const Visualizar = (props) => {
-
   const [id] = useState(props.match.params.id)
+  const [produto, setProduto] = useState(null)
 
   useEffect(() => {
     const getProduto = async () => {
-      await fetch("http://localhost/produtos-back/visualizar.php?id=" + id)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson)
-      });
+      const response = await fetch("http://localhost/produtos-back/visualizar.php?id=" + id)
+      const responseJson = await response.json();
+      setProduto(responseJson.mensagem)
     };
     getProduto();
   }, [id]);
-  return <h1>Visualizar</h1>;
+
+  return(
+    <div>
+      <h1>{produto?.titulo}</h1>
+      <h1>{produto?.descricao}</h1>
+    </div>
+  )
 };
