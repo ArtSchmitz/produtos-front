63a4f5ef94c0c59175c94f4b4c784a41a3066fbb
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-06-18 19:56:25 -0300

    vizu page

--- src/pages/Visualizar/index.jsx
+++ src/pages/Visualizar/index.jsx
@@ -1,22 +1,38 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Products } from "./styles";
 
 export const Visualizar = (props) => {
-  const [id] = useState(props.match.params.id)
-  const [produto, setProduto] = useState(null)
+  const [id] = useState(props.match.params.id);
+  const [produto, setProduto] = useState(null);
+  const [error, setError] = useState();
 
   useEffect(() => {
     const getProduto = async () => {
-      const response = await fetch("http://localhost/produtos-back/visualizar.php?id=" + id)
+      const response = await fetch(
+        "http://localhost/produtos-back/visualizar.php?id=" + id
+      );
       const responseJson = await response.json();
-      setProduto(responseJson.mensagem)
+      console.log(responseJson);
+      setProduto(responseJson.mensagem);
+      setError(responseJson.erro);
     };
     getProduto();
   }, [id]);
 
-  return(
-    <div>
-      <h1>{produto?.titulo}</h1>
-      <h1>{produto?.descricao}</h1>
-    </div>
-  )
+  if (error === false) {
+    return (
+      <Container>
+        <Products>
+          <h2>
+            <span>Titulo:</span> {produto?.titulo}
+          </h2>
+          <p>
+            <span>Descrição:</span> {produto?.descricao}
+          </p>
+        </Products>
+      </Container>
+    );
+  } else {
+    return <h2>Produto não encontrado</h2>;
+  }
 };
--- src/pages/Visualizar/styles.js
+++ src/pages/Visualizar/styles.js
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 960px;
+    margin: 50px auto;
+    box-shadow: 0 0 1em #6c757d;
+`;
+
+export const Products = styled.div`
+
+    margin: 1em;
+
+    h2{
+        font-size: 24px;
+    }
+
+    p{
+        font-size: 22px;
+    }
+
+    span{
+        font-size: 26px;
+        font-weight: 700;
+        color:#8e8b8b;
+    }
+`;
