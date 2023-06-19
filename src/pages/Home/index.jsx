152449bb75e8f4a4d4496bfb80472328fac29807
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-06-18 21:07:33 -0300

    ajustes front

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,25 +14,26 @@ export const Home = () => {
 
   const deleteProduct = (e, id) => {
     e.preventDefault();
-    console.log("clicando botão")
-      fetch('http://localhost/produtos-back/apagar.php', {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ produto: { id: id } }),
+    console.log("clicando botão");
+    fetch("http://localhost/produtos-back/apagar.php", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ produto: { id: id } }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          alert("Erro ao apagar o produto");
+        } else {
+          alert("Produto apagado com sucesso");
+          window.location.reload();
+        }
       })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.error) {
-            alert('Erro ao apagar o produto');
-          } else {
-            alert('Produto apagado com sucesso');
-          }
-        })
-        .catch((error) => {
-          console.error('Erro ao apagar o produto:', error);
-        });
+      .catch((error) => {
+        console.error("Erro ao apagar o produto:", error);
+      });
   };
 
   useEffect(() => {
@@ -42,7 +43,7 @@ export const Home = () => {
   return (
     <Container>
       <RegisterButton>
-      <Titulo>Listar</Titulo>
+        <Titulo>Listar</Titulo>
         <Link to="/cadastrar">
           <Button>Cadastre</Button>
         </Link>
@@ -64,8 +65,11 @@ export const Home = () => {
               <td>{produto.descricao}</td>
               <td>
                 <Link to={"/visualizar/" + produto.id}>Visualizar</Link>
-                 Editar 
-                 <button onClick={(e) => deleteProduct(e, produto.id)}>Apagar</button></td>
+                <Link to={"/editar/" + produto.id}>Editar</Link>
+                <button onClick={(e) => deleteProduct(e, produto.id)}>
+                  Apagar
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
